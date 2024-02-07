c894c27eb3445c1ab168f3c8f88f0858c1bc462a
austinc512 <cello.austin1994@gmail.com>
2024-02-07 11:53:15 -0600

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,73 @@
-import React from 'react';
+import * as esbuild from 'esbuild-wasm';
+import React, { useState, useEffect, useRef } from 'react';
+
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+
+const App = () => {
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+  const cols = 80;
+  const rows = 20;
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    // never attempt to do transpiling unless we're 100% sure
+    // we've initialized our service
+    if (!ref.current) {
+      return;
+    }
+    // else do thing
+    console.log(ref.current);
+
+    // const result = await ref.current.transform(input, {
+    //   loader: 'jsx',
+    //   target: 'es2015',
+    // });
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
+    });
+    // console.log(result);
+    setCode(result.outputFiles[0].text);
+  };
+  return (
+    <div>
+      <textarea
+        name=""
+        id=""
+        cols={cols}
+        rows={rows}
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
+
+// ReactDOM.render(<App/>, document.querySelector('#root'));
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,7 +78,7 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
+
+// CommonJS: require, module.exports
+// ES Modules: import, export
