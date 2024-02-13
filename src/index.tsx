7d4a72b5a837105716a51dadc8a452b1f6234534
austinc512 <cello.austin1994@gmail.com>
2024-02-13 10:35:58 -0600

    working on iframe implementation

--- src/index.tsx
+++ src/index.tsx
@@ -43,6 +43,11 @@ const App = () => {
     });
     // console.log(result);
     setCode(result.outputFiles[0].text);
+    try {
+      eval(result.outputFiles[0].text);
+    } catch (err) {
+      console.log(err);
+    }
   };
   return (
     <div>
@@ -58,6 +63,7 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
+      <iframe src="test.html" sandbox="allow-same-origin" />
     </div>
   );
 };
