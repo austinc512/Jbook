55f717fc01fe9a8072f8df40d14a0938e86baec2
austinc512 <cello.austin1994@gmail.com>
2024-02-15 17:18:28 -0600

    bug fix for some potential code execution that could break app

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ const App = () => {
   const ref = useRef<any>();
   const iframe = useRef<any>();
   const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
+  // const [code, setCode] = useState('');
   const cols = 80;
   const rows = 20;
 
@@ -29,6 +29,8 @@ const App = () => {
     if (!ref.current) {
       return;
     }
+
+    iframe.current.srcdoc = html;
 
     console.log(ref.current);
 
@@ -54,7 +56,13 @@ const App = () => {
       <div id="root"></div>
       <script>
         window.addEventListener('message', (event) => {
-          eval(event.data)
+          try {
+            eval(event.data)
+          } catch (err) {
+            const root = document.querySelector('#root');
+            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+            console.error(err);
+          }
         }, false);
       </script>
     </body>
@@ -73,12 +81,11 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
       <iframe
         ref={iframe}
         sandbox="allow-scripts"
         srcDoc={html}
-        title="child-code-execution"
+        title="code-preview"
       />
     </div>
   );
